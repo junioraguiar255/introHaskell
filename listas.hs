1da5d4530045c30abb032c536756130757b47562
Josemar <junioraguiar255@gmail.com>
2019-04-19 12:07:18 -0300

    Se um numero pertence a uma lista

--- listas.hs
+++ listas.hs
@@ -30,3 +30,10 @@ invlista :: [t] -> [t]
 invlista [] = []
 invlista(x:xs) = inv_lista xs ++[x]
 
+-- saber se um numero pertence a uma lista
+pertence :: [Int] -> Int -> Bool
+pertence [] _ = False
+pertence (x:xs) n |(x==n) = True
+				  |otherwise = pertence xs n
+
+
