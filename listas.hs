48a02aee199ba9d0295a89c1b4bc4525c0584c60
Josemar <junioraguiar255@gmail.com>
2019-04-19 16:41:09 -0300

    Recebe uma lista e verifica se todos são pares

--- listas.hs
+++ listas.hs
@@ -41,3 +41,9 @@ maior :: [Int] -> Int
 maior [x] = x
 maior (x:xs) |(x > maior xs) = x
 			 |otherwise = maior xs
+
+--recebe uma lista e retorna se todos os elementos são pares
+todos_pares :: [Int]->Bool
+todos_pares[] = True
+todos_pares (x:xs) |(mod x 2 == 1) = False
+			       |otherwise = todos_pares xs
