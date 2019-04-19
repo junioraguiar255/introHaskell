66104410c007411f02d6e04a1cd9bb9da0ae9833
Josemar <junioraguiar255@gmail.com>
2019-04-18 22:02:18 -0300

    Modificado a resolução da contagem de algarismo em um numero - compilando 100%

--- conta.hs
+++ conta.hs
@@ -6,24 +6,23 @@
 --conta_algarismos n = if n < 100 && n > 9 then 2
 --else then 1
 
+--TESTE
+-- Ex: n = 120
+-- conta = 120 10 1
+-- conta = se 120 > 10 entao qtd de algarismo é 1
+-- senao conta = 120 100 2
+-- conta se 120 > 100 entao qtd de algarismo é 2
+-- senao conta = 120 1000 3
+-- conta se 120 > 1000 entao qtd de algarismo é 3
+-- resposta = 3	  
+
 
 --Recursao
 
-conta_algarismo::Int->Int
-conta_algarismos < 0 = 0
-conta_algarismos n = conta n 10 1
+conta_algarismo :: Int->Int
+conta_algarismo n = (conta n 10 1)
 
-conta::Int->Int->Int->Int
+conta :: Int->Int->Int->Int
 conta n1 calc qtd
-|n1 > calc == qtd
-|otherwise = conta n1 (calc*10) qtd+1
-	  
-	  --TESTE
-	  -- Ex: n = 120
-	  -- conta = 120 10 1
-	  -- conta = se 120 > 10 entao qtd de algarismo é 1
-	  -- senao conta = 120 100 2
-      -- conta se 120 > 100 entao qtd de algarismo é 2
-      -- senao conta = 120 1000 3
-      -- conta se 120 > 1000 entao qtd de algarismo é 3
-      -- resposta = 3	  
+   |n1 < calc = qtd
+   |otherwise = conta n1 (calc*10) qtd+1 
