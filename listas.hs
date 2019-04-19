6a6ad2638327a2bb148fdf0d561b58df1c350fdf
Josemar <junioraguiar255@gmail.com>
2019-04-19 13:47:50 -0300

    maior elemento de uma lista

--- listas.hs
+++ listas.hs
@@ -35,5 +35,9 @@ pertence :: [Int] -> Int -> Bool
 pertence [] _ = False
 pertence (x:xs) n |(x==n) = True
 				  |otherwise = pertence xs n
-
-
+				  
+--maior elemento de uma lista
+maior :: [Int] -> Int
+maior [x] = x
+maior (x:xs) |(x > maior xs) = x
+			 |otherwise = maior xs
